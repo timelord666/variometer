6f7e1289fdb50244f5f742e81cc99bcae92c0040
timelord666 <34927031+timelord666@users.noreply.github.com>
2020-01-14 05:09:49 +0500

    add libraries

---
+++ libraries/Adafruit_BMP3XX_Library/examples/bmp3xx_simpletest/bmp3xx_simpletest.ino
@@ -0,0 +1,72 @@
+/***************************************************************************
+  This is a library for the BMP3XX temperature & pressure sensor
+
+  Designed specifically to work with the Adafruit BMP388 Breakout
+  ----> http://www.adafruit.com/products/3966
+
+  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
+  to interface.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BMP3XX bmp; // I2C
+//Adafruit_BMP3XX bmp(BMP_CS); // hardware SPI
+//Adafruit_BMP3XX bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
+unsigned long time = millis();
+float alt = bmp.readAltitude(SEALEVELPRESSURE_HPA);
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("BMP388 test");
+
+  if (!bmp.begin()) {
+    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+    while (1);
+  }
+
+  // Set up oversampling and filter initialization
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_NO_OVERSAMPLING);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_DISABLE);
+  bmp.setOutputDataRate(BMP3_ODR_200_HZ);
+  
+}
+
+void loop() {
+  // if (! bmp.performReading()) {
+  //   Serial.println("Failed to perform reading :(");
+  //   return;
+
+  // }
+
+
+  time = millis();
+  alt = bmp.readAltitude(SEALEVELPRESSURE_HPA);
+  Serial.println('$');
+  Serial.println(alt);
+  Serial.println(';');
+
+  Serial.println(millis() - time);
+
+
+  
+}
